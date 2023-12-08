244405f56c20650989bac62cd72b5166299c8795
Noise-Wall <107452784+Noise-Wall@users.noreply.github.com>
2023-12-07 21:40:47 -0700

    Integrar acciones deslizables
    Se agregó la librería "Slidable" para hacer botones de acción
    deslizables a las cartas de grupo.

--- lib/componentes/carta_grupo.dart
+++ lib/componentes/carta_grupo.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class GrupoCarta extends StatelessWidget {
   final String nombreGrupo;
@@ -18,54 +19,75 @@ class GrupoCarta extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.all(25.0),
-      child: Container(
-        padding: const EdgeInsets.all(24),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          border: Border.all(color: Colors.indigo, width: 2.0),
-          borderRadius: BorderRadius.circular(12),
-          boxShadow: const [
-            BoxShadow(
-              color: Colors.black12,
-              spreadRadius: 2,
-              blurRadius: 2,
-              offset: Offset(0, 2),
-            )
+      child: Slidable(
+        endActionPane: ActionPane(
+          motion: const ScrollMotion(),
+          children: [
+            SlidableAction(
+              onPressed: (context) => print("Slidable pressed"),
+              icon: Icons.edit_rounded,
+              backgroundColor: Colors.cyan.shade300,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            SlidableAction(
+              onPressed: (context) => print("Slidable pressed"),
+              icon: Icons.delete_rounded,
+              backgroundColor: Colors.red.shade400,
+              borderRadius: BorderRadius.circular(12),
+            ),
           ],
         ),
-        child: Row(
-          children: [
-            Expanded(
-                child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(nombreGrupo,
-                    softWrap: true,
-                    maxLines: 2,
-                    style: TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.indigo.shade600,
-                    )),
-                Text(nombreMateria,
-                    softWrap: true,
-                    maxLines: 2,
-                    style: TextStyle(
-                      fontSize: 24.0,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.indigo.shade600,
-                    )),
-                Text(
-                    "$numAlumnos alumnos, ${dias.fold(0, (previous, element) => element == true ? previous + 1 : previous)} dias a la semana",
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
-                      color: Colors.black26,
-                    )),
-              ],
-            ))
-          ],
+        child: Container(
+          padding: const EdgeInsets.all(24),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.indigo, width: 2.0),
+            borderRadius: BorderRadius.circular(12),
+            boxShadow: const [
+              BoxShadow(
+                color: Colors.black12,
+                spreadRadius: 2,
+                blurRadius: 2,
+                offset: Offset(0, 2),
+              )
+            ],
+          ),
+          child: Row(
+            children: [
+              Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(nombreGrupo,
+                          softWrap: true,
+                          maxLines: 2,
+                          style: TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.indigo.shade600,
+                          )),
+                      Text(nombreMateria,
+                          softWrap: true,
+                          maxLines: 2,
+                          style: TextStyle(
+                            fontSize: 24.0,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.indigo.shade600,
+                          )),
+                      Text(
+                          "$numAlumnos alumnos, ${dias.fold(0, (previous, element) => element == true ? previous + 1 : previous)} dias a la semana",
+                          overflow: TextOverflow.ellipsis,
+                          style: const TextStyle(
+                            color: Colors.black26,
+                          )),
+                    ],
+                  ),
+              ),
+            ],
+          ),
         ),
       ),
+
     );
   }
 }
