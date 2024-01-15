09f3fcc069045633e19727b8576d80b38ee1baeb
Noise-Wall <107452784+Noise-Wall@users.noreply.github.com>
2024-01-14 19:07:33 -0700

    Agrega opción de darle color al componente botón

--- lib/componentes/boton.dart
+++ lib/componentes/boton.dart
@@ -3,18 +3,21 @@ import 'package:flutter/material.dart';
 class Boton extends StatelessWidget {
   final String texto;
   VoidCallback onPresionado;
+  Color? colorBoton;
 
   Boton({
     super.key,
     required this.texto,
     required this.onPresionado,
+    this.colorBoton,
   });
 
   @override
   Widget build(BuildContext context) {
     return MaterialButton(
       onPressed: onPresionado,
-      color: Colors.indigo,
+      color: colorBoton ?? Colors.indigo,
+      shape: const StadiumBorder(),
       child: Text(texto),
       textColor: Colors.white,
     );
