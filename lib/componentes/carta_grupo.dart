b75088f4bebe11287ec27162b26f0816647f0c6f
Noise-Wall <107452784+Noise-Wall@users.noreply.github.com>
2024-01-14 19:20:15 -0700

    Implementa funciones del Slidable
    Se completaron los botones de edición y eliminación para los
    Slidable en la página Grupos, haciendo posible la edición y
    eliminación de los mismos.

--- lib/componentes/carta_grupo.dart
+++ lib/componentes/carta_grupo.dart
@@ -1,22 +1,36 @@
+import 'package:control_asistencias/componentes/boton.dart';
+import 'package:control_asistencias/componentes/modal.dart';
+import 'package:control_asistencias/data/modelos/grupos.dart';
+import 'package:control_asistencias/data/controladores/ctrl_grupos.dart';
+import 'package:control_asistencias/paginas/pagina_principal/grupos_add.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 
 class GrupoCarta extends StatelessWidget {
+  final int idGrupo;
   final String nombreGrupo;
   final String nombreMateria;
+  final String turno;
+
   final int numAlumnos;
   final List<bool> dias;
+  final Future<void> Function(dynamic) refresh;
 
   const GrupoCarta({
     super.key,
+    required this.idGrupo,
     required this.nombreGrupo,
     required this.nombreMateria,
+    required this.turno,
     required this.numAlumnos,
     required this.dias,
+    required this.refresh,
   });
 
   @override
   Widget build(BuildContext context) {
+    bool _confirmarBorrado = false;
+
     return Container(
       padding: const EdgeInsets.all(25.0),
       child: Slidable(
@@ -24,13 +38,71 @@ class GrupoCarta extends StatelessWidget {
           motion: const ScrollMotion(),
           children: [
             SlidableAction(
-              onPressed: (context) => print("Slidable pressed"),
+              onPressed: (context) => Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => GrupoAdd(
+                      grupo: Grupo(
+                          idGrupo: idGrupo,
+                          nombreGrupo: nombreGrupo,
+                          nombreMateria: nombreMateria,
+                          turno: turno)),
+                ),
+              ).then(refresh),
               icon: Icons.edit_rounded,
               backgroundColor: Colors.cyan.shade300,
               borderRadius: BorderRadius.circular(12),
             ),
             SlidableAction(
-              onPressed: (context) => print("Slidable pressed"),
+              onPressed: (contexto) => Modal(
+                  contexto,
+                  Column(
+                    children: [
+                      const Text(
+                        "ADVERTENCIA",
+                        style: TextStyle(
+                          color: Colors.redAccent,
+                          fontWeight: FontWeight.w900,
+                          fontSize: 25,
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                      const Text(
+                        '''Esta acción eliminará el grupo y toda la información que contenga.
+¿Realmente desea borrar el grupo?
+Presione \"Sí, borrar\" dos veces para borrar el grupo.
+                        ''',
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                        textAlign: TextAlign.left,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Boton(
+                            texto: "Sí, borrar",
+                            onPresionado: () {
+                              if (!_confirmarBorrado) {
+                                _confirmarBorrado = true;
+                              } else {
+                                _confirmarBorrado = false;
+                                CtrlGrupos().deleteGrupo(idGrupo);
+                                Navigator.of(context).pop();
+                              }
+                            },
+                            colorBoton: Colors.red,
+                          ),
+                          Boton(
+                              texto: "Cancelar",
+                              onPresionado: () {
+                                _confirmarBorrado = false;
+                                Navigator.of(context).pop();
+                              }),
+                        ],
+                      ),
+                    ],
+                  )).then(refresh),
               icon: Icons.delete_rounded,
               backgroundColor: Colors.red.shade400,
               borderRadius: BorderRadius.circular(12),
@@ -55,39 +127,47 @@ class GrupoCarta extends StatelessWidget {
           child: Row(
             children: [
               Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(nombreGrupo,
-                          softWrap: true,
-                          maxLines: 2,
-                          style: TextStyle(
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.indigo.shade600,
-                          )),
-                      Text(nombreMateria,
-                          softWrap: true,
-                          maxLines: 2,
-                          style: TextStyle(
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.indigo.shade600,
-                          )),
-                      Text(
-                          "$numAlumnos alumnos, ${dias.fold(0, (previous, element) => element == true ? previous + 1 : previous)} dias a la semana",
-                          overflow: TextOverflow.ellipsis,
-                          style: const TextStyle(
-                            color: Colors.black26,
-                          )),
-                    ],
-                  ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(nombreGrupo,
+                        softWrap: true,
+                        maxLines: 2,
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.indigo.shade600,
+                        )),
+                    Text(nombreMateria,
+                        softWrap: true,
+                        maxLines: 2,
+                        style: TextStyle(
+                          fontSize: 24.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.indigo.shade600,
+                        )),
+                    Text(turno,
+                        softWrap: true,
+                        maxLines: 2,
+                        style: TextStyle(
+                          fontSize: 19.0,
+                          fontWeight: FontWeight.w300,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.indigo.shade300,
+                        )),
+                    Text(
+                        "$numAlumnos alumnos, ${dias.fold(0, (previous, element) => element == true ? previous + 1 : previous)} dias a la semana",
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                          color: Colors.black26,
+                        )),
+                  ],
+                ),
               ),
             ],
           ),
         ),
       ),
-
     );
   }
 }
