9d54966a46474841910e740adf98c3f3b66e4915
Noise-Wall <107452784+Noise-Wall@users.noreply.github.com>
2024-01-23 10:36:01 -0700

    Anexo de main.dart al commit anterior

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,20 @@
+import 'dart:io';
+
 import 'package:control_asistencias/paginas/pagina_principal.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import "package:sqflite_common_ffi/sqflite_ffi.dart";
 
 void main() async {
   // Carga archivo de variables ambientales, como hashes, contrasenas y otros.
   // Por su naturaleza sensible, este archivo no se sube al GitHub.
   // Si no tienen el archivo .env, pidanmelo.
+  if (Platform.isWindows || Platform.isLinux) {
+   sqfliteFfiInit();
+  }
+
+  databaseFactory = databaseFactoryFfi;
+
   await dotenv.load(fileName: ".env");
   runApp(const MyApp());
 
